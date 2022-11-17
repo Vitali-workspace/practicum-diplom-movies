b91b97f5c3d18eda269688f10935f0b4519dd5bf
vitali <vitali.slepnyov@gmail.com>
2022-11-17 23:55:54 +0300

    feat: добавить схему user

---
+++ movies-explorer-api/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    maxlength: 30,
+    minlength: 2,
+  },
+})
+
+module.exports = mongoose.model('user', userSchema);
